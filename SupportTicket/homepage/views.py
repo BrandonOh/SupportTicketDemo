1aeef8960625bc20976d04294d82c09a1e02a7f1
BrandonOh <brandonoh2938@gmail.com>
2024-09-03 14:56:02 -0400

    Custom login form and page made.

--- SupportTicket/homepage/views.py
+++ SupportTicket/homepage/views.py
@@ -1,4 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate, login
+from .script.forms import LoginForm
+from django.contrib import messages
+
+def loginUser(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.success(request, f'Welcome, {user.username}!')
+                return redirect('index')
+            else:
+                messages.error(request, 'Invalid username or password.')
+    else:
+        form = LoginForm()
+    return render(request, 'homepage/login.html', {'form': form})
 
 def index(request):
-    return render(request, 'homepage/login.html')
+    return render(request, 'homepage/index.html')
